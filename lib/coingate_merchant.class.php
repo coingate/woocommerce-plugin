ea8e99ac0578d80a25af436d1bfaef95747c7815
Irmantas Baciulis <i.baciulis@gmail.com>
2015-07-23 09:42:04 +0300

    Update coingate php merchant class to 1.0.2

--- lib/coingate_merchant.class.php
+++ lib/coingate_merchant.class.php
@@ -1,86 +1,104 @@
 <?php
 
+/**
+ * PHP CoinGate Merchant Class
+ *
+ * @author    CoinGate <info@coingate.com>
+ * @version   1.0.2
+ * @link      http://developer.coingate.com  
+ * @license   MIT
+ */
+
+define('CLASS_VERSION', '1.0.2');
+
 class CoingateMerchant {
-	private $app_id 	= '';
-	private $api_key 	= '';
-	private $api_secret = '';
-	private $mode 		= 'sandbox'; // live or sandbox
-	private $version	= 'v1';
-	private $api_url 	= '';
+    private $app_id             = '';
+    private $api_key            = '';
+    private $api_secret         = '';
+    private $mode               = 'sandbox'; // live or sandbox
+    private $version            = 'v1';
+    private $api_url            = '';
+    private $user_agent         = '';
+    private $user_agent_origin  = 'CoinGate PHP Merchant Class';
 
-	public function __construct($options = array())
-	{
-		foreach($options as $key => $value)
-		{
-			if (in_array($key, array('app_id', 'api_key', 'api_secret', 'mode')))
-			{
-				$this->$key = trim($value);
-			}			
-		}
+    public function __construct($options = array())
+    {
+        foreach($options as $key => $value)
+        {
+            if (in_array($key, array('app_id', 'api_key', 'api_secret', 'mode', 'user_agent')))
+            {
+                $this->$key = trim($value);
+            }
 
-		$this->set_api_url();
-	}
+            if (empty($this->user_agent)) {
+                $this->user_agent = $this->user_agent_origin . ' v' . CLASS_VERSION;
+            }
+        }
 
-	public function create_order($params = array())
-	{
-		$this->request('/orders', 'POST', $params);
-	}
+        $this->set_api_url();
+    }
 
-	public function get_order($order_id)
-	{
-		$this->request('/orders/' . $order_id);
-	}
+    public function create_order($params = array())
+    {
+        $this->request('/orders', 'POST', $params);
+    }
 
-	public function request($url, $method = 'GET', $params = array())
-	{
-		$url 		= $this->api_url . $url;
-		$nonce      = $this->nonce();
-		$message    = $nonce . $this->app_id . $this->api_key;
-		$signature  = hash_hmac('sha256', $message, $this->api_secret);
+    public function get_order($order_id)
+    {
+        $this->request('/orders/' . $order_id);
+    }
 
-		$headers 	= array();
-		$headers[]	= 'Access-Key: ' . $this->api_key;
-		$headers[]	= 'Access-Nonce: ' . $nonce;
-		$headers[]	= 'Access-Signature: ' . $signature;
+    public function request($url, $method = 'GET', $params = array())
+    {
+        $url        = $this->api_url . $url;
+        $nonce      = $this->nonce();
+        $message    = $nonce . $this->app_id . $this->api_key;
+        $signature  = hash_hmac('sha256', $message, $this->api_secret);
 
-		$curl = curl_init();
-		
-		$curl_options = array(
-			CURLOPT_RETURNTRANSFER  => 1,
-			CURLOPT_URL             => $url
-		);
+        $headers    = array();
+        $headers[]  = 'Access-Key: ' . $this->api_key;
+        $headers[]  = 'Access-Nonce: ' . $nonce;
+        $headers[]  = 'Access-Signature: ' . $signature;
 
-		if ($method == 'POST')
-		{
-			$headers[] = 'Content-Type: application/x-www-form-urlencoded';
+        $curl = curl_init();
+        
+        $curl_options = array(
+            CURLOPT_RETURNTRANSFER  => 1,
+            CURLOPT_URL             => $url
+        );
 
-			array_merge($curl_options, array(CURLOPT_POST => 1));
-			curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($params));
-		}
+        if ($method == 'POST')
+        {
+            $headers[] = 'Content-Type: application/x-www-form-urlencoded';
 
-		curl_setopt_array($curl, $curl_options);
-		curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+            array_merge($curl_options, array(CURLOPT_POST => 1));
+            curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($params));
+        }
 
-		$response       = curl_exec($curl);
-		$http_status    = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+        curl_setopt_array($curl, $curl_options);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($curl, CURLOPT_USERAGENT, $this->user_agent);
 
-		curl_close($curl);
+        $response       = curl_exec($curl);
+        $http_status    = curl_getinfo($curl, CURLINFO_HTTP_CODE);
 
-		$this->success		= $http_status == 200;
-		$this->status_code 	= $http_status;
-		$this->response 	= $response;
-	}
+        curl_close($curl);
 
-	private function nonce()
-	{
-		return phpversion() >= 5 ? str_replace('.', '', ((string) microtime(true))) : time();
-	}
+        $this->success      = $http_status == 200;
+        $this->status_code  = $http_status;
+        $this->response     = $response;
+    }
 
-	private function set_api_url()
-	{
-		$this->api_url = strtolower($this->mode) == 'live' ? 'https://coingate.com/api/' : 'https://sandbox.coingate.com/api/';
-		$this->api_url .= $this->version;
-	}
+    private function nonce()
+    {
+        return time();
+    }
+
+    private function set_api_url()
+    {
+        $this->api_url = strtolower($this->mode) == 'live' ? 'https://coingate.com/api/' : 'https://sandbox.coingate.com/api/';
+        $this->api_url .= $this->version;
+    }
 }
 
 ?>
